5e2d00a988093c336b6b015ac1420714de5ae9ed
huyinghuan <ec.huyinghuan@gmail.com>
2018-08-16 08:41:22 +0800

    update build

--- README.md
+++ README.md
@@ -4,11 +4,14 @@
 
 ```
 mgtv install -g mb
+
 #在项目目录下
 mb version -m 'message'
+
+-m 提交信息 可选
 ```
 
-mb 会运行 `pacakge.json`里面的`scripts`的`build`命令。请务必将编译好的项目文件输出到`build`文件夹下。
+mb 会运行 `pacakge.json`里面的`scripts`的`build`命令。请务必将编译好的项目文件输出到`dist`文件夹下。
 
 mb 会自动创建一个 `mb_xxx`到tag，并且`push`到远程服务器
 
--- build.sh
+++ build.sh
@@ -1,33 +1,66 @@
 #!/bin/bash
 set -e
-version=$1
-if [ "$1" == "" ];then
+VERSION=""
+OUTPUT=""
+COMMIT=""
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -o|--output)
+    OUTPUT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -m|--commit)
+    COMMIT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
+VERSION="$1"
+if [ "$VERSION" == "" ];then
     echo "版本号不能为空"
     exit 1
 else
-    version=$1_mb
-fi
-commit=""
-if [ $# ] && [ "$2" == "-m" ] && [ "$3" != "" ];then
-    commit="$3"
-fi
-if [ "$commit" == "" ];then
-    commit="tag verion to ${version}"
+    VERSION=${VERSION}_mb
 fi
 
+if [ "${COMMIT}" == "" ];then
+    COMMIT="tag verion to ${VERSION}"
+fi
+
+if [ "$OUTPUT" == "" ];then
+    OUTPUT="dist"
+fi
+
+
 projectName=${PWD##*/}
-tempBranchName=build_${projectName}_${version}
+tempBranchName=build_${projectName}_${VERSION}
 currentBranchName=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
-echo version: $version
+
 echo workspace: $PWD
 echo project: $projectName
+echo version: $VERSION
 echo current branch: $currentBranchName
+echo output: $OUTPUT
+
 npm run build
 git checkout -b $tempBranchName
 echo -e "node_modules\n.*" > .gitignore
 for entry in "$PWD"/*
 do
-  if [ "$entry" != "$PWD/build" ] && [ "$entry" != "$PWD/node_modules" ] && [ "$entry" != "$PWD/.gitignore" ]; then
+  if [ "$entry" != "$PWD/$OUTPUT" ] && [ "$entry" != "$PWD/node_modules" ] && [ "$entry" != "$PWD/.gitignore" ]; then
     rm -rf "$entry"
   fi
 done
@@ -37,10 +70,10 @@ for f in .[^.]*; do
     fi
 done
 git add .
-git commit -m "$commit"
-git tag "$version" -m "$commit"
+git commit -m "$COMMIT"
+git tag "$VERSION" -m "$COMMIT"
 git checkout $currentBranchName
 git checkout .gitignore
 git branch -D $tempBranchName
 git push --tags
-echo tag to $version finish
+echo tag to $VERSION finish
